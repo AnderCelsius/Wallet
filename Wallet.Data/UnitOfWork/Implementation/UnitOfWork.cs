fe06dda7ec5cb1b7064bc7555be44db641279d39
obinna asiegbulam <oasiegbulam@gmail.com>
2022-06-06 22:37:19 +0100

    Added generic repository and unitofwork

---
+++ Wallet.Data/UnitOfWork/Implementation/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Data.Repositories.Implementations;
+using Wallet.Data.Repositories.Interfaces;
+using Wallet.Data.UnitOfWork.Interface;
+using Wallet.Models;
+
+namespace Wallet.Data.UnitOfWork.Implementation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly WalletDbContext _context;
+        private IGenericRepository<Product> _products;
+        private IGenericRepository<UserWallet> _wallets;
+        public UnitOfWork(WalletDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Returns a new instance of GenericRepositoy<Product> if _products is null
+        /// </summary>
+        public IGenericRepository<Product> Products => _products ??= new GenericRepository<Product>(_context);
+
+
+        /// <summary>
+        /// Returns a new instance of GenericRepositoy<UserWallet> if _wallets is null
+        /// </summary>
+        public IGenericRepository<UserWallet> Wallets => _wallets ??= new GenericRepository<UserWallet>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Wallet.Data/UnitOfWork/Interface/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Data.Repositories.Interfaces;
+using Wallet.Models;
+
+namespace Wallet.Data.UnitOfWork.Interface
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Product> Products { get; }
+        IGenericRepository<UserWallet> Wallets { get; }
+
+        Task Save();
+    }
+}
